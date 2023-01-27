b3a948e1c801313fba40e7eaa1285da8643be38c
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2023-01-27 20:18:20 +0800

    index php

--- index.php
+++ index.php
@@ -13,11 +13,6 @@
 </head>
 <body>
 
-    <!-- Here Insert PHP -->
-    <?php
-    
-
-    ?>
     <div class="main">
         <header>
             <div class="logo"><span>Hopi Crafts</span></div>
@@ -33,7 +28,40 @@
                 </ul>
             </div>
         </header>
+        
+        <div class="png-carousel" id="home">
+            <h1>Art Collection for the Month</h1>
+            <img class="carousel-img" src="sources/img/Chibi Art/Base 11_20220717232125(1).png" alt="#">
+            <img class="carousel-img" src="sources/img/Chibi Art/Base11-001_20220726033356(1).png" alt="#">
+            <img class="carousel-img" src="sources/img/Chibi Art/Base11-001_20220726033439(1).png" alt="#">
+            <!-- <img class="carousel-img" src="sources/img/Chibi Art/Base 11_20220717231344.png" alt="#">
+            <img class="carousel-img" src="sources/img/Chibi Art/Base 11_20220717231344.png" alt="#">
+            <img class="carousel-img" src="sources/img/Chibi Art/Base 11_20220717231344.png" alt="#"> -->
+        </div>
 
+        <div class="collections" id="collections">
+            <h1>Collections</h1>
+            <p>Here are some of my works</p>
+        </div>
+
+        <div class="shop">
+            <h1>Shop</h1>
+            <p>Order your art and it will be delivered in your front door</p>
+        </div>
+
+        <div class="commission">
+            <h1>Check out what we offer!!!</h1>
+            <p>Custom made arts or crafts tinker in your liking</p>
+
+        </div>
+    
+        <div class="FAQ">
+            <h1>About Hopi Crafts</h1>
+        </div>
+
+        <footer>
+            
+        </footer>
     </div>
 </body>
 </html>
