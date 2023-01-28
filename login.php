75ede80f242b4bc901cd8bd9e6fe71e309e04595
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2023-01-28 17:20:00 +0800

    login

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <!-- PHP code here -->
-<?php 
-
+<?php
+    $username = "";
+    $password = "";
+    $name = "";
 ?>
 
 <!-- html -->
@@ -13,6 +15,19 @@
     <title>Log In</title>
 </head>
 <body>
-    
+<div class="container">
+    <form action="login.php" method="post">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username">
+      <br>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name">
+      <br>
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password">
+      <br>
+      <input type="submit" value="Login">
+    </form>
+  </div>
 </body>
 </html>
