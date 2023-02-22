b357652f498a4addb9675e8def515b5ba8070623
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-22 18:33:06 +0800

    12

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hopi Crafts</title>
     <script src="script.js"></script> 
-    <link rel="icon" type="image/png" href="sources\img\logo.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="sources/img/logo.png" sizes="16x16">
     <link rel="stylesheet" href="style.css">
 
 
