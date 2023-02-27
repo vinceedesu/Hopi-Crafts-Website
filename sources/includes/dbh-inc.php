fbd6af4c27902737cf77e8bda1e34e1760f17959
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-27 23:46:19 +0800

    23

--- sources/includes/dbh-inc.php
+++ sources/includes/dbh-inc.php
@@ -6,11 +6,14 @@ $username = "root";
 $password = "";
      
 
-$conn = mysqli_connect(hostname:$host, 
+$conn = new mysqli(hostname:$host, 
                        username:$username, 
                        password:$password, 
                        database:$dbname);
 
-if ($conn->connect_error) {
-    die("Connection error: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection error: " . mysqli_connect_error());
 }
+
+else{
+    echo "Connected successfully";}
--- sources/php/sign-up.php
+++ sources/php/sign-up.php
@@ -1,18 +1,48 @@
 <?php
 
-include('../includes/dbh.inc.php');
+include  __DIR__ . '../includes/dbh-inc.php';
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$pass = $_POST['pwd'];
-$pass2 = $_POST['pwd2'];
+if(isset($_POST['submit'])){
 
-$sql = "SELECT * FROM user_form WHERE email = '$email' && password = '$pass'";
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pass = md5($_POST['pwd']);
+    $pass2 = md5($_POST['pwd2']);
+    $user_type = '';
 
-if(mysqli_num_rows($result) > 0){
-    
-    $error[]= "user already exists";
+    $select = "SELECT * FROM user_form WHERE email = '$email' && password = '$pass'";
+
+    $result = $conn->query($select);
+
+    if(mysqli_num_rows($result) > 0){
+        
+        $error[]= "user already exists";
+
+    }else{
+        if($pass != $pass2){
+            $error[] = "passwords do not match";
+
+        }else{
+            $pass = password_hash($pass, PASSWORD_DEFAULT);
+            $insert = "INSERT INTO user_form (name, email, password, user_type) VALUES ('$name', '$email', '$pass', '$user_type')";
+            mysqli_query($conn, $insert);
+            header("Location: signup-success.php");
+        }
+    }
 }
+// $name = $_POST[''];
+// $email = $_POST['email'];
+// $pass = $_POST['pwd'];
+// $pass2 = $_POST['pwd2'];
+
+
+// $sql = "SELECT * FROM user_form WHERE email = '$email' && password = '$pass'";
+
+// if(mysqli_num_rows($result) > 0){
+    
+//     $error[]= "user already exists";
+
+// }
 
 
 
@@ -55,8 +85,8 @@ if(mysqli_num_rows($result) > 0){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../style.css">
-    <link rel="stylesheet" href="../css/no-scroll.css">
+    <!-- <link rel="stylesheet" href="../../style.css"> -->
+    <!-- <link rel="stylesheet" href="../css/no-scroll.css"> -->
     <link rel="icon" type="image/png" href="../img/logo.png" sizes="16x16">
     <title>Sign Up</title>
 </head>
