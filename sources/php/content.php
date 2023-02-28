50b25224cc5bf416e50e0e08f3454153565abab4
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-28 20:16:38 +0800

    eee

---
+++ sources/php/content.php
@@ -0,0 +1,38 @@
+<div class="png-carousel" id="home">
+                <h1>< Art Collection for the Month ></h1>
+                <p>They made the Art for the month for a reason</p>
+                <img class="carousel-img" src="..\img\Line Art\FA - HJS_20220423014918.png" alt="#">
+                <img class="carousel-img" src="../img/Chibi Art/Base 11_20220717232125(1).png" alt="#">
+                <img class="carousel-img" src="..\img\Line Art\Untitled6_20220406014651.png" alt="#">
+            </div>
+
+            
+            <div class="collections" id="collections">
+                <h1>< Collections ></h1>
+                <p>Explore our Wide Variety of Art Collections</p>
+                <img class="carousel-img" src="../img/Line Art/Untitled19_20211219222934.png" alt="#">
+                <img class="carousel-img" src="..\img\Crafts\IMG20230105031819.jpg" alt="#">
+                <img class="carousel-img" src="../img/Chibi Art/Chibi OC 2_20220718014643(2).png" alt="#">
+    
+            </div>
+            
+            <div class="shop" id="shop">
+                <div class="column left">
+                    <h1>< Shop ></h1>
+                    <p>Order your art and it will be delivered in your front door</p>
+                    <a href="..\html\shop.html"><button class="click">Visit Shop</button></a>
+                </div>
+                <div class="column right">
+                    <img class="shop-img" src="..\img\Line Art\Untitled31_20220421025406.png" alt="#"><br>
+                </div>
+            </div>
+
+            <div class="about-us" id="about-us">
+                <div class="column to-left">
+                <img src="..\img\logo.png" alt="#">
+                </div>
+                <div class="column to-right">
+                    <h1>About Hopi Crafts</h1>
+                    <p>Welcome to Hopi Craft Website, where I showcase my passion for art and creativity. I am a dedicated artist who believes in the power of art to transform lives and bring joy to the world. My goal is to inspire others through my artwork and to share my love of art with the world</p>
+                </div>
+            </div>
