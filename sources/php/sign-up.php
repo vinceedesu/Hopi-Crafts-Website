a005be5130a402e19b04ab5053470e7686a57da3
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-28 18:06:23 +0800

    Done sa sign up

--- sources/php/sign-up.php
+++ sources/php/sign-up.php
@@ -1,92 +1,11 @@
-<?php
-
-// include __DIR__ . '../includes/dbh.inc.php';
-
-// if(isset($_POST['submit'])){
-
-//     $name = $_POST['name'];
-//     $email = $_POST['email'];
-//     $pass = md5($_POST['pwd']);
-//     $pass2 = md5($_POST['pwd2']);
-//     $user_type = '';
-
-//     $select = "SELECT * FROM user_form WHERE email = '$email' && password = '$pass'";
-
-//     $result = $conn->query($select);
-
-//     if(mysqli_num_rows($result) > 0){
-        
-//         $error[]= "user already exists";
-
-//     }else{
-//         if($pass != $pass2){
-//             $error[] = "passwords do not match";
-
-//         }else{
-//             $pass = password_hash($pass, PASSWORD_DEFAULT);
-//             $insert = "INSERT INTO user_form (name, email, password, user_type) VALUES ('$name', '$email', '$pass', '$user_type')";
-//             mysqli_query($conn, $insert);
-//             header("Location: signup-success.php");
-//         }
-//     }
-// }
-// $name = $_POST[''];
-// $email = $_POST['email'];
-// $pass = $_POST['pwd'];
-// $pass2 = $_POST['pwd2'];
-
-
-// $sql = "SELECT * FROM user_form WHERE email = '$email' && password = '$pass'";
-
-// if(mysqli_num_rows($result) > 0){
-    
-//     $error[]= "user already exists";
-
-// }
-
-
-
-// @include '../includes/dbh.inc.php';
-
-// if(isset($_POST['submit'])){
-
-//     $name = $_POST['name'];
-//     $email = $_POST['email'];
-//     $pass = md5($_POST['pwd']);
-//     $pass2 = md5($_POST['pwd2']);
-//     $user_type = $_POST['user_type'];
-
-//     $select = "SELECT * FROM user_form WHERE email = '$email' && password = '$pass'";
-
-//     $result = mysqli_query($conn, $select);
-
-//     if(mysqli_num_rows($result) > 0){
-        
-//         $error[]= "user already exists";
-
-//     }else{
-//         if($pass != $pass2){
-//             $error[] = "passwords do not match";
-
-//         }else{
-//             $pass = password_hash($pass, PASSWORD_DEFAULT);
-//             $insert = "INSERT INTO user_form (name, email, password, user_type) VALUES ('$name', '$email', '$pass', '$user_type')";
-//             mysqli_query($conn, $insert);
-//             header("Location: signup-success.php");
-//         }
-//     }
-// }
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="../../style.css"> -->
-    <!-- <link rel="stylesheet" href="../css/no-scroll.css"> -->
+    <link rel="stylesheet" href="../../style.css">
+    <link rel="stylesheet" href="../css/no-scroll.css">
     <link rel="icon" type="image/png" href="../img/logo.png" sizes="16x16">
     <title>Sign Up</title>
 </head>
@@ -109,9 +28,25 @@
             <section class="signUp-form">
                 <form action="../includes/signup.inc.php" method="post" novalidate>
                     <?php
-                        if(isset($error)){
-                            foreach($error as $error){
-                                echo '<p class="error">'.$error.'</p>';
+                        // Check if there is any error message
+                        if (isset($_GET["error"])) {
+                            if ($_GET["error"] == "emptyinput") {
+                                echo "<p>Fill in all fields!</p>";
+                            }
+                            else if ($_GET["error"] == "invalidemail") {
+                                echo "<p>Choose a proper email!</p>";
+                            }
+                            else if ($_GET["error"] == "passwordsdontmatch") {
+                                echo "<p>Passwords don't match!</p>";
+                            }
+                            else if ($_GET["error"] == "stmtfailed") {
+                                echo "<p>Something went wrong, try again!</p>";
+                            }
+                            else if ($_GET["error"] == "emailalreadyexists") {
+                                echo "<p>Email already exists!</p>";
+                            }
+                            else if ($_GET["error"] == "none") {
+                                echo "<p>You have signed up!</p>";
                             }
                         }
                     ?>
