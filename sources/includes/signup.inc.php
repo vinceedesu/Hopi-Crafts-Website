48afc452343a51f693a076a59aeaf19736806ca4
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-02-28 16:58:38 +0800

    test

--- sources/includes/signup.inc.php
+++ sources/includes/signup.inc.php
@@ -1,4 +1,35 @@
 <?php
+// check if the user submitted the form
+if(isset($_POST['submit'])){
+
+    // include the database connection file
+    include_once 'dbh.inc.php';
+
+    // get values from the form
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pass = md5($_POST['pwd']);
+    $pass2 = md5($_POST['pwd2']);
+    $user_type = '';
+
+    // check if the user already exists
+    $select = "SELECT * FROM user_form WHERE email = '$email' && password = '$pass'";
+
+    $result = mysqli_query($conn, $select);
+
+    if(mysqli_num_rows($result) > 0){
+        $error[]= "user already exists";
+    }else{
+        if($pass != $pass2){
+            $error[] = "passwords do not match";
+        }else{
+            $pass = password_hash($pass, PASSWORD_DEFAULT);
+            $insert = "INSERT INTO user_form (name, email, password, user_type) VALUES ('$name', '$email', '$pass', '$user_type')";
+            mysqli_query($conn, $insert);
+            header("Location: ../php/signup-success.php");
+        }
+    }
+}
 
 // if (empty($_POST["name"])) {
 //     die("Name is required"); 
